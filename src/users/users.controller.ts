eae80459df2f793f17b51522a6478ba283b7a6f8
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-02-08 19:55:25 +0330

    Edit file user.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,34 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  
+  constructor(private readonly userService:UsersService) {}
+
+  @Post()
+  create(@Body createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get()
+  findAll(){
+    return this.userService.findAllUser();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.viewUser(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() UpdateUserDto: UpdateUserDto) {
+    return this.userService.updateUser(+id, UpdateUserDto);
+  }
+
+  @Delete(':id') 
+  remove(@Param('id') id: string) {
+    return this.userService.removeUser(+id);
+  }
 }
