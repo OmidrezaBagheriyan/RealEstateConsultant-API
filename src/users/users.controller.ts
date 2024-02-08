5b8e112459ae08134b8fc379874f5ad03d41f18c
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-02-08 19:28:28 +0330

    Clear users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,41 +8,5 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(
-    @InjectRepository(User) private readonly userRepository: Repository<User>,
-  ) {}
-
-  createUser(createUserDto:CreateUserDto):Promise<User> {
-    const user: User = new User();
-    user.name = createUserDto.name;
-    user.age = createUserDto.age;
-    user.email = createUserDto.email;
-    user.username = createUserDto.username;
-    user.password = createUserDto.password;
-    user.gender = createUserDto.gender;
-    return this.userRepository.save(user);
-  }
-
-  findAllUser():Promise<User[]> {
-      return this.userRepository.find();
-  }
-
-  viewUser(id: number): Promise<User>{
-    return this.userRepository.findOneBy({ id });
-  }
-
-  updateUser(id: number, updateUserDto:UpdateUserDto): Promise<User>{
-    const user:User = new User();
-    user.name = updateUserDto.name;
-    user.age = updateUserDto.age;
-    user.email = updateUserDto.email;
-    user.username = updateUserDto.username;
-    user.password = updateUserDto.password;
-    user.id = id;
-    return this.userRepository.save(user);
-  }
-
-  remove(id: number): Promise<{ affected?: number }>{
-    return this.userRepository.delete(id);
-  }
+  
 }
