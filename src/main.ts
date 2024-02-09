e28d35f75bb428281a06f21f44d806a60079bfe5
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-02-10 01:42:49 +0330

    Add swagger in project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const swagger = new DocumentBuilder()
+    .setTitle('Real estate consultant - API')
+    .setDescription('There are documents related to "Real estate consultant", we use it for further communication')
+    .setVersion('0.1')
+    .addTag('real-estate-consultant')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('api', app, document)
+  
   await app.listen(3000);
 }
 bootstrap();
