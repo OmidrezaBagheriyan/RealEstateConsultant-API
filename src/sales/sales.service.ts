6d450e13d5f0091991d66e7e771542050065b830
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-02-10 01:17:02 +0330

    Edit file sale.service.ts

--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -1,26 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Sale } from './entities/sale.entity';
+import { Repository } from 'typeorm';
 import { CreateSaleDto } from './dto/create-sale.dto';
 import { UpdateSaleDto } from './dto/update-sale.dto';
 
 @Injectable()
 export class SalesService {
-  create(createSaleDto: CreateSaleDto) {
-    return 'This action adds a new sale';
+  constructor(
+    @InjectRepository(Sale) private readonly saleRepository:Repository<Sale>,
+  ) { }
+
+  createSale(createSaleDto:CreateSaleDto):Promise<Sale>{
+    const sale: Sale = new Sale();
+    sale.title = createSaleDto.title;
+    sale.price = createSaleDto.price;
+    sale.meter = createSaleDto.meter;
+    sale.price_per_meter = createSaleDto.price_per_meter;
+    sale.number_of_rooms = createSaleDto.number_of_rooms;
+    sale.floor = createSaleDto.floor;
+    sale.unit = createSaleDto.unit;
+    sale.address = createSaleDto.address;
+    sale.description = createSaleDto.description;
+    return this.saleRepository.save(sale)
   }
 
-  findAll() {
-    return `This action returns all sales`;
+  findAllSale():Promise<Sale[]> {
+    return this.saleRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} sale`;
+  viewSale(id: number): Promise<Sale> {
+    return this.saleRepository.findOneBy({ id });
   }
 
-  update(id: number, updateSaleDto: UpdateSaleDto) {
-    return `This action updates a #${id} sale`;
+  updateSale(id: number, updateSaleDto:UpdateSaleDto):Promise<Sale> {
+      const sale: Sale = new Sale();
+      sale.title = updateSaleDto.title;
+      sale.price = updateSaleDto.price;
+      sale.meter = updateSaleDto.meter;
+      sale.price_per_meter = updateSaleDto.price_per_meter;
+      sale.number_of_rooms = updateSaleDto.number_of_rooms;
+      sale.floor = updateSaleDto.floor;
+      sale.unit = updateSaleDto.unit;
+      sale.address = updateSaleDto.address;
+      sale.description = updateSaleDto.description;
+      sale.id = id;
+      return this.saleRepository.save(sale)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} sale`;
+  removeSale(id: number): Promise<{ affected?: number }> {
+    return this.saleRepository.delete(id);
   }
 }
