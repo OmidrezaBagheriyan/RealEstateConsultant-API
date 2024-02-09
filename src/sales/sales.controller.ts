fc31ae6794a6927e8866c97c912e1ca59c1815c0
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-02-10 01:27:49 +0330

    Add file sales.controller.ts

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -5,30 +5,30 @@ import { UpdateSaleDto } from './dto/update-sale.dto';
 
 @Controller('sales')
 export class SalesController {
-  constructor(private readonly salesService: SalesService) {}
+  constructor(private readonly saleService:SalesService) {}
 
   @Post()
-  create(@Body() createSaleDto: CreateSaleDto) {
-    return this.salesService.create(createSaleDto);
+  create(@Body() createSaleDto:CreateSaleDto){
+    return this.saleService.createSale(createSaleDto);
   }
 
   @Get()
-  findAll() {
-    return this.salesService.findAll();
+  findAll(){
+    return this.saleService.findAllSale();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.salesService.findOne(+id);
+  findOne(@Param('id') id: string){
+    return this.saleService.viewSale(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSaleDto: UpdateSaleDto) {
-    return this.salesService.update(+id, updateSaleDto);
+  update(@Param('id') id: string, @Body() updateSaleDto:UpdateSaleDto){
+    return this.saleService.updateSale(+id, updateSaleDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.salesService.remove(+id);
+  remove(@Param('id') id: string){
+    return this.saleService.removeSale(+id);
   }
 }
