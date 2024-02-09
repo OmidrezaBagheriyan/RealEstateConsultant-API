1ce728b9e6151288538e213fd33f4d6b3b0b531d
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-02-10 01:11:19 +0330

    Edit file create-sale.dto.ts

--- src/sales/dto/create-sale.dto.ts
+++ src/sales/dto/create-sale.dto.ts
@@ -1,1 +1,39 @@
-export class CreateSaleDto {}
+import { IsInt, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreateSaleDto {
+    @IsString()
+    @MinLength(10, {
+        message: 'Title must have atleast 2 characters.'
+    })
+    @IsNotEmpty()
+    title: string;
+    
+    @IsInt()
+    price:number;
+
+    @IsInt()
+    meter:number;
+
+    @IsInt()
+    price_per_meter:number;
+    
+    @IsInt()
+    number_of_rooms:number;
+
+    @IsInt()
+    floor:number;
+    
+    @IsInt()
+    unit:number;
+
+    @IsString()
+    @MinLength(10, {
+        message: 'Title must have atleast 2 characters.'
+    })
+    @IsNotEmpty()
+    address: string;
+
+    @IsString()
+    description: string;
+
+}
