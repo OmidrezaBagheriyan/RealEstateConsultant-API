14ba6ac3be83570f0656acb8edd951f185e44b44
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-02-09 03:59:15 +0330

    Create sale entity

--- src/sales/entities/sale.entity.ts
+++ src/sales/entities/sale.entity.ts
@@ -1,1 +1,34 @@
-export class Sale {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Sale {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 64 })
+    title: string;
+
+    @Column({ type: 'int'})
+    price: number;
+
+    @Column({ type: 'float'})
+    meter: number;
+
+    @Column({ type: 'int'})
+    price_per_meter: number;
+
+    @Column({ type: 'int'})
+    number_of_rooms:number;
+
+    @Column({ type: 'int'})
+    floor: number;
+
+    @Column({ type: 'int'})
+    unit: number;
+
+    @Column({ type: 'varchar', length: 256 })
+    address: string;
+
+    @Column({ type: 'varchar', length: 512 })
+    description: string;
+}
