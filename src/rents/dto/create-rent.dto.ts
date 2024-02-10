02a7fd4cf43f3986954bf064242648b9fb7c08df
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-02-10 04:39:18 +0330

    Add file create-rent.dto.ts

--- src/rents/dto/create-rent.dto.ts
+++ src/rents/dto/create-rent.dto.ts
@@ -1,1 +1,38 @@
-export class CreateRentDto {}
+import { IsInt, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreateRentDto {
+    @IsString()
+    @MinLength(10, {
+        message: 'Title must have atleast 2 characters.'
+    })
+    @IsNotEmpty()
+    title: string;
+    
+    @IsInt()
+    price:number;
+
+    @IsInt()
+    meter:number;
+
+    @IsInt()
+    rental_price:number;
+    
+    @IsInt()
+    number_of_rooms:number;
+
+    @IsInt()
+    floor:number;
+    
+    @IsInt()
+    unit:number;
+
+    @IsString()
+    @MinLength(10, {
+        message: 'Title must have atleast 2 characters.'
+    })
+    @IsNotEmpty()
+    address: string;
+
+    @IsString()
+    description: string;
+}
