422245e634e469e12552388ddde367d22de787fb
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-02-10 04:36:27 +0330

    Add file rent.entity.ts

--- src/rents/entities/rent.entity.ts
+++ src/rents/entities/rent.entity.ts
@@ -1,1 +1,34 @@
-export class Rent {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Rent {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 64 })
+    title: string;
+
+    @Column({ type: 'int'})
+    price: number;
+
+    @Column({ type: 'int'})
+    meter: number;
+
+    @Column({ type: 'int'})
+    rental_price: number;
+
+    @Column({ type: 'int'})
+    number_of_rooms: number;
+
+    @Column({ type: 'int'})
+    floor: number;
+
+    @Column({ type: 'int'})
+    unit: number;
+
+    @Column({ type: 'varchar', length: 256 })
+    address: string;
+
+    @Column({ type: 'varchar', length: 512 })
+    description: string;
+}
