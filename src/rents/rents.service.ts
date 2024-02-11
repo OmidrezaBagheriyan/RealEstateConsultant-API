b1ea498f4741c7a1d0c4deaa353d889216cdb4bd
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-02-11 13:14:25 +0330

    Add file rents.service.ts

--- src/rents/rents.service.ts
+++ src/rents/rents.service.ts
@@ -1,26 +1,54 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRentDto } from './dto/create-rent.dto';
 import { UpdateRentDto } from './dto/update-rent.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Rent } from './entities/rent.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class RentsService {
-  create(createRentDto: CreateRentDto) {
-    return 'This action adds a new rent';
+  constructor(
+    @InjectRepository(Rent) private readonly rentRepository:Repository<Rent>,
+  ) {}
+
+  createRent(createRentDto:CreateRentDto):Promise<Rent>{
+    const rent:Rent = new Rent()
+    rent.title = createRentDto.title;
+    rent.price = createRentDto.price;
+    rent.meter = createRentDto.meter;
+    rent.rental_price = createRentDto.rental_price;
+    rent.number_of_rooms = createRentDto.number_of_rooms;
+    rent.floor = createRentDto.floor;
+    rent.unit = createRentDto.unit;
+    rent.address = createRentDto.address;
+    rent.description = createRentDto.description;
+    return this.rentRepository.save(rent)
   }
 
-  findAll() {
-    return `This action returns all rents`;
+  findAllSale():Promise<Rent[]>{
+    return this.rentRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} rent`;
+  viewRent(id: number):Promise<Rent>{
+    return this.rentRepository.findOneBy({ id });
   }
 
-  update(id: number, updateRentDto: UpdateRentDto) {
-    return `This action updates a #${id} rent`;
+  updateRent(id: number, updateRentDto:UpdateRentDto):Promise<Rent>{
+    const rent:Rent = new Rent();
+    rent.title = updateRentDto.title;
+    rent.price = updateRentDto.price;
+    rent.meter = updateRentDto.meter;
+    rent.rental_price = updateRentDto.rental_price;
+    rent.number_of_rooms = updateRentDto.number_of_rooms;
+    rent.floor = updateRentDto.floor;
+    rent.unit = updateRentDto.unit;
+    rent.address = updateRentDto.address;
+    rent.description = updateRentDto.description;
+    rent.id = id;
+    return this.rentRepository.save(rent);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} rent`;
+  removeRent(id: number):Promise<{ affected?: number}>{
+    return this.rentRepository.delete(id);
   }
 }
