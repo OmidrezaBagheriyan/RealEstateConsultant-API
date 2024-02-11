9faf35ef0b13607a838e9684ec9d0540847acea1
Omidreza Bagheriyan <omidcoder21@gmail.com>
2024-02-11 12:53:21 +0330

    Add TyoeOrmModule in rents.module.ts

--- src/rents/rents.module.ts
+++ src/rents/rents.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RentsService } from './rents.service';
 import { RentsController } from './rents.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Rent } from './entities/rent.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Rent])],
   controllers: [RentsController],
   providers: [RentsService],
 })
